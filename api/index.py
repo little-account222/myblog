687a2aed2246cc99038aa4fb48dc7bd549a2f1f7
little-account222 <3860798565@qq.com>
2025-04-12 19:25:18 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,38 +1,7 @@
-from flask import Flask, request, jsonify
-import requests
+from flask import Flask,render_template
 
-app = Flask(__name__)
+app = Flask()
+@app.get('/')
+def home():
+    return render_template('home.html')
 
-def run_ocr_job():
-    url = 'https://ocrcodesimpleonlyocr.vercel.app/ocr/'
-
-    data = {
-        'firstName': 'Deep',
-        'lastName': 'Gohil',
-        'mobileNumber': '08104680835',
-        'email': 'deepgohil777@gmail.com',
-        'date': '02-202-03'
-    }
-
-    try:
-        with open('test1.jpeg', 'rb') as image_file:
-            files = {'image': ('test1.jpeg', image_file, 'image/jpeg')}
-            response = requests.post(url, data=data, files=files)
-
-            if response.status_code == 200:
-                return {"status": "success", "data": response.json()}
-            else:
-                return {"status": "error", "code": response.status_code, "message": response.text}
-    except Exception as e:
-        return {"status": "error", "message": str(e)}
-
-@app.route("/trigger", methods=["GET"])
-def trigger():
-    return jsonify(run_ocr_job())
-
-# Required for Vercel Deployment
-def handler(request):
-    return app(request, None)
-
-if __name__ == "__main__":
-    app.run(debug=True)
