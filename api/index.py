e7a91f530eb23846496eb82ea75f7c818ca1fea9
little-account222 <3860798565@qq.com>
2025-04-13 20:06:14 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,6 +3,6 @@ from flask import Flask, render_template
 # 正确实例化 Flask 应用
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/page/home')
 def home():
-    return render_template('home.html')
+    return render_template('stop.html')
